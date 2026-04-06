6c076da39afec2e292c601b3b529c73523775985
Majid Hussain <majidrajput246@gmail.com>
2026-04-06 14:05:17 +0500

    38 to 53

---
+++ javascript/switch revision/app.js
@@ -0,0 +1,66 @@
+// switch statement 
+
+// let dayOfWeek = 'Mon';
+
+// const today = new Date();
+// const day = today.getDay();
+// let dayOfWeek;
+// console.log(day)
+
+// if(day === 1){
+//   dayOfWeek = 'Mon';
+// }else if(day === 2){
+//   dayOfWeek = 'Tue';
+// }else if(day === 3){
+//   dayOfWeek = 'Wed';
+// }else if(day === 4){
+//   dayOfWeek = 'Thu';
+// }else if(day === 5){
+//   dayOfWeek = 'Fri';
+// }else if(day === 6){
+//   dayOfWeek = 'Sat';
+// }else if(day === 0){
+//   dayOfWeek = 'Sun';
+// }
+
+// switch (dayOfWeek) {
+//     case 'Sat':
+//         console.log('Whoopee')
+//         break;
+//     case 'Sun':
+//         console.log('Whoopee')
+//         break;
+//     default:
+//         console.log('So Sad its working day')
+//         break;
+// };
+
+
+// =====================================================================
+
+// while loop 
+
+// let i = 0;
+
+// while (i <= 5) {
+//     alert(i);
+//     i++;
+// };
+
+// let i = 0;
+// do {
+//     alert(i);
+//     i++;
+// }while (i <= 5);
+
+// let i = 10;
+// do {
+//     alert(i);
+//     i++;
+// }while(i <= 5)
+
+// let i = 10;
+// while (i <= 5) {
+//     alert(i);
+//     i++;
+// };
